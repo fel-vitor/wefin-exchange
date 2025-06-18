b29f97982e1f596626b9e29aed5fc0197d48ac0a
Vitor Fel <vitorfel2000@gmail.com>
2025-06-18 16:33:39 -0300

    chore: Instala e configura json-server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
+import { provideHttpClient } from '@angular/common/http';
 import {
-	ApplicationConfig,
-	provideExperimentalZonelessChangeDetection,
-	provideZoneChangeDetection,
-} from "@angular/core";
-import { provideRouter } from "@angular/router";
-
-import { routes } from "./app.routes";
+  type ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideExperimentalZonelessChangeDetection(),
-		provideRouter(routes),
-	],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    { provide: LOCALE_ID, useValue: 'pt' },
+  ],
 };
