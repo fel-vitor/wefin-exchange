90dcfde803acbf87f538a7c2b6a39b91db9f2e17
Vitor Fel <vitorfel2000@gmail.com>
2025-06-19 18:03:23 -0300

    feat(currency-rate): Cria serviço

---
+++ src/app/shared/services/currency-rate/currency-rate.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CurrencyRateInterface } from '@shared/interfaces/currency.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CurrencyRateService {
+  private _http = inject(HttpClient);
+
+  private readonly baseURI = '/api/exchangeRates';
+
+  getAll() {
+    return this._http.get<CurrencyRateInterface[]>(this.baseURI);
+  }
+
+  getById(id: string) {
+    return this._http.get<CurrencyRateInterface>(`${this.baseURI}/${id}`);
+  }
+
+  post(payload: CurrencyRateInterface) {
+    return this._http.post<CurrencyRateInterface>(`${this.baseURI}/`, payload);
+  }
+
+  put(id: string, payload: CurrencyRateInterface) {
+    return this._http.put<CurrencyRateInterface>(
+      `${this.baseURI}/${id}`,
+      payload
+    );
+  }
+
+  patch(id: string, payload: Partial<CurrencyRateInterface>) {
+    return this._http.patch<CurrencyRateInterface>(
+      `${this.baseURI}/${id}`,
+      payload
+    );
+  }
+
+  delete(id: string) {
+    return this._http.delete<CurrencyRateInterface>(`${this.baseURI}/${id}`);
+  }
+}
