0417867c07bb800c6c5a183454d13ebea94da7fa
Vitor Fel <vitorfel2000@gmail.com>
2025-06-19 15:00:38 -0300

    feat(home): Cria componente

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'wefin-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+}
