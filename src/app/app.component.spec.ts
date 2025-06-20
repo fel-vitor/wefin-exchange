30c0d0c0b9fe35fab17eda9b535e209bbf8c6495
Vitor Fel <vitorfel2000@gmail.com>
2025-06-20 09:49:48 -0300

    fix: Corrige providers dos testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        MessageService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        DialogService,
+      ],
     }).compileComponents();
   });
 
